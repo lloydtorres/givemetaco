cc53b39c272ddabcd0617a872e67ea9e6c1230ca
lloydtorres <lloyd.torres95@gmail.com>
2015-10-04 15:32:01 -0400

    Remove db reference

--- server.js
+++ server.js
@@ -5,16 +5,9 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===========================================
-    
-// config files
-var db = require('./config/db');
 
 // set our port
 var port = process.env.PORT || 8080; 
-
-// connect to our mongoDB database 
-// (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
